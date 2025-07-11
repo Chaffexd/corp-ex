a762678425327529e7f995890e047a1f536921c6
Chaffexd <shanechaffe@gmail.com>
2025-07-11 13:58:05 +0100

    add live preview

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,14 @@
+import { ContentfulLivePreviewProvider } from "@contentful/live-preview/react";
+
 import "@/styles/globals.css";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <ContentfulLivePreviewProvider
+      locale="en-US"
+      enableLiveUpdates={pageProps.draftMode}
+    >
+      <Component {...pageProps} />
+    </ContentfulLivePreviewProvider>
+  );
 }
--- pages/example/[slug].js
+++ pages/example/[slug].js
@@ -1,7 +1,7 @@
-import Image from "next/image";
-import Head from "next/head";
 import { createClient } from "contentful";
 import { componentMap } from "@/components/ContentBlocks";
+import { useContentfulLiveUpdates } from "@contentful/live-preview/react";
+
 
 const client = createClient({
   space: "ne1qvy5mlpq6",
@@ -9,21 +9,16 @@ const client = createClient({
   host: "preview.contentful.com",
 });
 
+
 export default function Home({ pageData }) {
   console.log("Entry Retrieved =", pageData.items[0]);
-  const data = pageData.items[0].fields;
+
+  const rawEntry = pageData.items[0];
+  const liveEntry = useContentfulLiveUpdates(rawEntry); 
+  const data = liveEntry?.fields || {};
 
   return (
     <>
-      {" "}
-      <Head>
-        <meta
-          httpEquiv="Cache-Control"
-          content="no-cache, no-store, must-revalidate"
-        />
-        <meta httpEquiv="Pragma" content="no-cache" />
-        <meta httpEquiv="Expires" content="0" />
-      </Head>
       <article className="m-auto w-full flex flex-col items-center justify-center mt-20">
         <h1 className="text-center text-5xl font-bold max-w-[570px]">
           {data.title}
