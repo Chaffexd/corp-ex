6cccecf302ba8d6dd6a43dea682548aaadd79068
Chaffexd <shanechaffe@gmail.com>
2025-07-11 19:49:48 +0100

    adjust headers

--- next.config.mjs
+++ next.config.mjs
@@ -4,6 +4,23 @@ const nextConfig = {
   images: {
     domains: ["images.ctfassets.net"],
   },
+  headers: async () => {
+    return [
+      {
+        source: "/(.*)",
+        headers: [
+          {
+            key: "Cache-Control",
+            value: "no-store, no-cache, must-revalidate, proxy-revalidate",
+          },
+          {
+            key: "Pragma",
+            value: "no-cache",
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
